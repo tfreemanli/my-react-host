dd6aae18dfcfe0ad1045bd3e9f82797a01bcebd0
Freeman <43063030+tfreemanli@users.noreply.github.com>
2023-12-14 13:30:25 +1300

    Ongoing

--- src/index.js
+++ src/index.js
@@ -1,20 +1,42 @@
-import { useRef } from "react";
-import ReactDOM from "react-dom/client";
+import { useState } from 'react';
+import ReactDOM from 'react-dom/client';
 
-function App() {
-  const inputElement = useRef();
+function MyForm() {
+  const [inputs, setInputs] = useState({});
 
-  const focusInput = () => {
-    inputElement.current.focus();
-  };
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs(values => {return {...values, [name] : value}});
+ }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(inputs);
+  }
 
   return (
-    <>
-      <input type="text" ref={inputElement} />
-      <button onClick={focusInput}>Focus Input</button>
-    </>
-  );
+    <form onSubmit={handleSubmit}>
+      <label>Enter your name:
+      <input 
+        type="checkbox" 
+        name="username" 
+        checked={inputs.checked} 
+        
+      />
+      </label>
+      <label>Enter your age:
+        <input 
+          type="number" 
+          name="age" 
+          value={inputs.age || ""} 
+          onChange={handleChange}
+        />
+        </label>
+        <input type="submit" />
+    </form>
+  )
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(<MyForm />);
