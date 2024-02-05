120135f49c4d0268bd7fd8fd8bf5712d2850804d
Freeman <43063030+tfreemanli@users.noreply.github.com>
2024-02-06 01:59:31 +1300

    CSS the home page with animation and img

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,16 @@
+import { Link } from "react-router-dom";
 
 const Home =  () => {
 
 	return (
-		<div className="row body-section flex-v-middle flex-h-middle">
-			<p>Welcome! @center</p>			
+		<div className="row body-section flex-v-middle home-bg">
+			<div className="home-text-container">
+				<p className="home-text-hdr1">Knowledge Base 101</p>
+				<p className="home-text-normal">Wise man said, "An old photo tells more than a good memory."</p>
+				<Link to="./notes" className="home-btn1">My Notes &gt;&gt;</Link>
+				<Link to="./blogs" className="home-btn2">Blogs &gt;&gt;</Link>
+			</div>	
+			<div className="home-img"></div>
 		</div>
 	);
 
